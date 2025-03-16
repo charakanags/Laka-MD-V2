8d907360022d711f13642337a90a5dc1aba21d5a
charakanags <repomax074@gmail.com>
2025-03-16 13:52:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=c5BDxBKb#QCC2b-RuzJYfRY15g5ccorOQ96eyzy5v3NPaUj2wF68",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
